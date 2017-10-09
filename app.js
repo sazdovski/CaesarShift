42d6391767a11c880cbc2f1315b7e6d36f186eef
Sazdovski <sazdovski96@gmail.com>
2017-10-09 04:15:42 +0200

    v2.0

--- app.js
+++ app.js
@@ -1,49 +1,83 @@
 
-function sync()
+function syncFirst()
 {
-
-  var n1 = document.getElementById('textToEncrypt');
-  var n2 = document.getElementById('encrypted');
-
-//Get value from dropdown
+    var n1 = document.getElementById('firstInput');
+    var n2 = document.getElementById('secondInput');
+    //Get value from dropdown
     var selectedDropdown = document.getElementById("key");
     var key = selectedDropdown.options[selectedDropdown.selectedIndex].value;
 
-  n2.value = caesarShift(n1.value,key);
-};
+    n2.value = caesarEncrypt(n1.value,key);
+}
+
+function syncSecond(){
+
+    var n1 = document.getElementById('firstInput');
+    var n2 = document.getElementById('secondInput');
+    //Get value from dropdown
+    var selectedDropdown = document.getElementById("key");
+    var key = selectedDropdown.options[selectedDropdown.selectedIndex].value;
+
+    n1.value = caesarDecrypt(n2.value,key);
+}
 
 
+function caesarEncrypt(str, amount) {
 
-function caesarShift(str, amount) {
-    
         var output = '';
-        
+
         for (var i = 0; i < str.length; i ++) {
-    
+
             // Getting current character
             var current = str[i];
 
-            // Checking if it is a letter. 
-            if (current.match(/[a-z]/i)) {
-    
                 // Get its ASCII value
                 var asciiValue = str.charCodeAt(i);
-                
-                // Encrypting uppercase letters
-                if ((asciiValue >= 65) && (asciiValue <= 90))
-                    current = String.fromCharCode((((asciiValue - 65) + parseInt(amount)) % 26) + 65);
-    
-                // Encrypting lowercase letters
-                else if ((asciiValue >= 97) && (asciiValue <= 122)){
-                   current = String.fromCharCode((((asciiValue - 97) + parseInt(amount)) % 26) + 97);
-                    
+
+                // Encrypting
+                if ((asciiValue >= 32) && (asciiValue <= 126))
+                    current = String.fromCharCode((((asciiValue - 32) + parseInt(amount)) % 95) + 32);
+
+                else {
+                    output = "Wrong Input";
                 }
-            }
-    
+
+
             // Appending everything
             output += current;
-    
+
         }
             return output;
-    
-    };
+
+    }
+
+
+
+function caesarDecrypt(str, amount) {
+
+    var output = '';
+
+    for (var i = 0; i < str.length; i ++) {
+
+        // Getting current character
+        var current = str[i];
+
+        // Get its ASCII value
+        var asciiValue = str.charCodeAt(i);
+
+        // Decrypting
+        if ((asciiValue >= 32) && (asciiValue <= 126))
+            current = String.fromCharCode((((asciiValue - 32) - parseInt(amount)) % 95) + 32);
+
+        else {
+            output = "Wrong Input";
+        }
+
+
+        // Appending everything
+        output += current;
+
+    }
+    return output;
+
+}
